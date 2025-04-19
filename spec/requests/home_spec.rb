56d55929f066b9d6f39acb2abc2235b69fc840e0
HunJer93 <jhunton22@gmail.com>
2025-04-19 13:40:27 -0400

    Added rspec and single test

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+  describe "GET /" do
+    it "returns http success" do
+      get "/"
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the home page" do
+      get "/"
+      expect(response.body).to include("Turbo chat")
+    end
+  end
+end
